1fb6f869b42392389bf98d341eb0e5233d3a8e15
Hugo Macedo de Carvalho <84153007+HMCarvalho-220@users.noreply.github.com>
2026-05-25 17:33:20 -0300

    rabbit

--- backend/app/config.py
+++ backend/app/config.py
@@ -12,6 +12,11 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
 
+    RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "rabbitmq")
+    RABBITMQ_PORT = int(os.environ.get("RABBITMQ_PORT", 5672))
+    RABBITMQ_USER = os.environ.get("RABBITMQ_USER", "guest")
+    RABBITMQ_PASS = os.environ.get("RABBITMQ_PASS", "guest")
+
 
 class TestingConfig(Config):
     TESTING = True
